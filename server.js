adbef14c8424c6b6fd4ff42d7c4a0bf191b38dc3
Brajesh Kurkure <118987844+Brajesh9373@users.noreply.github.com>
2025-04-08 21:07:52 +0530

    server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(✅ Server running on http://localhost:${PORT});
+  console.log(`✅ Server running on http://localhost:${PORT}`);
 });
 
 
